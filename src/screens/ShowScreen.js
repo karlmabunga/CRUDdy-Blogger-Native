f936d18646808a0c262496f6dfefca1a8d16368e
Karl Mabunga <karlmabunga@yahoo.com>
2021-05-04 12:11:35 -0700

    The search screen now shows the title of the blog post that is pressed on.

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
 
 const ShowScreen = ({ navigation }) => {
-  console.log(navigation.getParam('id'))
+  const { state } = useContext(Context);
+
+  const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
 
   return (
     <View>
-      <Text>Show Screen</Text>
+      <Text>{blogPost.title}</Text>
     </View>
   );
 };
