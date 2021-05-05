d436ee50a6005e2eafa01728a1cc1e1d120eb515
Karl Mabunga <karlmabunga@yahoo.com>
2021-05-05 12:11:01 -0700

    Added a feather plus icon from expo and directed the navigation to the create screen when pressing on it

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -29,6 +29,16 @@ const IndexScreen = ({ navigation }) => {
   );
 };
 
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+        <Feather name="plus" size={30} style={{ marginRight: 20 }} />
+      </TouchableOpacity>
+    ),
+  };
+};
+
 const styles = StyleSheet.create({
   row: {
     flexDirection: 'row',
