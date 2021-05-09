a2b5f8baba3c0d8a48636c9dd83a432d3b367f46
Karl Mabunga <karlmabunga@yahoo.com>
2021-05-08 21:29:06 -0700

    Edit functionality was added to the context and implemented in the edit screen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -5,13 +5,19 @@ import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation}) => {
 
-  const { state } = useContext(Context);
-  const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
+  const id = navigation.getParam('id');
+  const { state, editBlogPost } = useContext(Context);
+  const blogPost = state.find((blogPost) => blogPost.id === id);
 
 
 
   return (
-    <BlogPostForm />
+    <BlogPostForm
+    onSubmit={(title, content) => {
+      editBlogPost( id, title, content );
+    }}
+    initialValues={{ title: blogPost.title, content: blogPost.content }}
+    />
   )
 };
 
