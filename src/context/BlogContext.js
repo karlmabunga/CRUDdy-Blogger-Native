b324956047737ba3e905c07c7fbb79da3f569dfe
Karl Mabunga <karlmabunga@yahoo.com>
2021-05-10 14:20:54 -0700

    Delete function works with json server now

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -10,9 +10,7 @@ const blogReducer = (state, action) => {
         return blogPost.id === action.payload.id ? action.payload : blogPost;
       });
     case 'delete_blogpost':
-      return state.filter((blogPost) => blogPost.id !== action.payload)
-    case 'add_blogpost':
-      return [...state, { id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content }];
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     default:
       return state;
   }
@@ -28,7 +26,6 @@ const getBlogPosts = (dispatch) => {
 const addBlogPost = (dispatch) => {
   return async (title, content, callback) => {
     await jsonServer.post('/blogposts', { title, content });
-    // dispatch({ type: 'add_blogpost', payload: { title, content }});
     if (callback) {
       callback();
     };
@@ -36,7 +33,8 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: 'delete_blogpost', payload: id })
   }
 };
